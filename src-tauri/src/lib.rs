d0a554ca5f035b4303f71485f77e57bdf2b11d0d
galangMaulana01 <maulanagerbil99@gmail.com>
2026-06-11 11:23:58 +0700

    Fix Final: Clean build Gradle & siapkan Entry Point Rust

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,1 +1,6 @@
-// File formalitas untuk Tauri Android
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
